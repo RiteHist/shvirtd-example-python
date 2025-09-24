2db475a73795f3b30cb0be38206f673bbfc23f8c
RiteHist <greggthewright@gmail.com>
2025-09-24 10:28:32 +1000

    Update bash script

--- download.sh
+++ download.sh
@@ -1,7 +1,12 @@
 !#/bin/bash
 
-git clone https://github.com/RiteHist/shvirtd-example-python.git /opt/example-python
+if [ -d "/opt/example-python" ]; then
+    cd /opt/example-python
+    git pull
+else
+    git clone https://github.com/RiteHist/shvirtd-example-python.git /opt/example-python
+    cd /opt/example-python
+fi
 
-cd /opt/example-python
 
 docker compose up  -d
